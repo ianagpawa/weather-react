f478425ce8100a3a7e95ff7f29fd30c292cdf514
Ian Agpawa <agpawaji@gmail.com>
2017-12-18 22:29:34 -0500

    feat: Start event handler for search_bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    render () {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+
+    }
+
+    onInputChange(event){
+        console.log(event.target.value)
+    }
+
+    render() {
         return (
             <form className='input-group'>
-                <input />
+                <input
+                    className='form-control'
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    placeholder="Get a five-day forecast in your favorite cities"
+                />
                 <span className="input-group-btn">
                 <button type='submit' className='btn btn-secondary'>
                     Search
