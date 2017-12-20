feae0f6ac2bf99033daa4932a354d9163c1f0f52
Ian Agpawa <agpawaji@gmail.com>
2017-12-20 14:26:04 -0500

    refactor: Complete ajax action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axious from 'axious';
+
 import API_KEY from './client_secrets'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
 
@@ -6,7 +8,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
 
+    const request = axious.get(url);
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
