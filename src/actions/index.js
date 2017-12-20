ee239720c3c8efe6244cdd22d680fbb32daf3d70
Ian Agpawa <agpawaji@gmail.com>
2017-12-20 14:40:52 -0500

    feat: Add imports to searchBar.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import axious from 'axious';
+import axios from 'axios';
 
 import API_KEY from './client_secrets'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
@@ -8,7 +8,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
 
-    const request = axious.get(url);
+    const request = axios.get(url);
 
     return {
         type: FETCH_WEATHER,
