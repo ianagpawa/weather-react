9c4395c82718cc47c50c32ffc9608f7e211953eb
Ian Agpawa <agpawaji@gmail.com>
2017-12-22 23:08:37 -0500

    feat: Complete Google Map integration

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 
 class GoogleMap extends Component {
     componentDidMount() {
-        new.google.maps.Map(this.refs.map, {
+        new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {
                 lat: this.props.lat,
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
 
 
 
@@ -10,10 +11,11 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord; // cityData.city.coord.lon + cityData.city.coord.lat
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={temps} color='red' units='K'/></td>
                 <td><Chart data={pressures} color='green' units='hPa'/></td>
                 <td><Chart data={humidities} color='black' units='%'/></td>
