686ab329d824d30d202518d20339ea5a8a1a4c04
Ian Agpawa <agpawaji@gmail.com>
2017-12-22 21:53:31 -0500

    refactor: Changed width and heigth of chart to svg attributes

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,20 +6,19 @@ import Chart from "../components/chart";
 
 class WeatherList extends Component {
     renderWeather(cityData) {
-        const name = cityData.city.name
-        const temps = cityData.list.map(weather => weather.main.temp)
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-
-                <td><Chart data={temps} color='orange' /></td>
-                <td><Chart data={pressures} color='green' /></td>
-                <td><Chart data={humidities} color='red' /></td>
+                <td><Chart data={temps} color='red'/></td>
+                <td><Chart data={pressures} color='green'/></td>
+                <td><Chart data={humidities} color='black'/></td>
             </tr>
-        )
+        );
     }
 
     render () {
@@ -28,7 +27,7 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
+                        <th>Temp.</th>
                         <th>Pressure</th>
                         <th>Humidity</th>
                     </tr>
@@ -37,12 +36,12 @@ class WeatherList extends Component {
                     {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
-        )
+        );
     }
 }
 
-function matpStateToProps({ weather }){
+function mapStateToProps({ weather }){
     return { weather }; // { weather: weather} ES6 shortcut
 }
 
-export default connect(matpStateToProps)(WeatherList);
+export default connect(mapStateToProps)(WeatherList);
